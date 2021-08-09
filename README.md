3c16d6cedd0760e2f39cf25a9c75ea9ef6f88538
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-09 11:53:02 +0300

    generate linear dataset

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ML_linear_models
- ML_linear_models
+- dataset_generation.py - generation of datasets
+- normal equation - solution of linear regression with use of normal equation
---
+++ dataset_generation.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = 100 # number of samples
+
+def generate_linear_dataset(m):
+    X = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * X + np.random.randn(100, 1)
+    return X, y
+
+def plot_dataset(X, y):
+    plt.plot(X, y, 'o')
+    plt.show()
+
+X, y = generate_linear_dataset(100)
+plot_dataset(X, y)
