8c91fa463fe1e73906ba675364bf4fb033721531
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-09 12:51:00 +0300

    numpy gradient descent

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ML_linear_models
 - dataset_generation.py - generation of datasets
-- normal equation - solution of linear regression with use of normal equation
+- normal_equation.py - solution of linear regression with use of normal equation
+- gradient_descent.py - solution of linear regression with use of gradient descent
---
+++ gradient_descent.py
@@ -0,0 +1,17 @@
+import  numpy as np
+from dataset_generation import generate_linear_dataset
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    m=100
+    X, y = generate_linear_dataset(m)
+    X_b = np.c_[np.ones((100, 1)), X]  # add 1 for theta_0 in each X
+
+    eta = 0.1 # learning rate
+    n_iterations = 1000
+    theta = np.random.randn(2, 1) ## random initial theta
+    for iteration in range(n_iterations):
+        gradient = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+        theta = theta - eta * gradient
+    print('Solution with use of numpy:')
+    print(theta)
