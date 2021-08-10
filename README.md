d2b6da499611e71410dc935e60666990167be3d5
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-10 09:43:35 +0300

    elastic net regulrization

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 - normal_equation.py - solution of linear regression with use of normal equation
 - gradient_descent.py - solution of linear regression with use of gradient descent
 - stochastic_gradient_descent.py - stochastic gradient search and mini-batch gradient search
-- regularization.py - examples of ridge (add l2 norm of parameters vector to MSE) and lasso regularization (add l1 norm of parameters vector to MSE)
+- regularization.py - examples of 
+    - ridge (add l2 norm of parameters vector to MSE)
+    - lasso regularization (add l1 norm of parameters vector to MSE)
+    - elastic net regularization - mix of both
--- regulariztion.py
+++ regulariztion.py
@@ -1,5 +1,5 @@
 from dataset_generation import generate_parabola_dataset, plot_dataset
-from sklearn.linear_model import Ridge, SGDRegressor, Lasso
+from sklearn.linear_model import Ridge, SGDRegressor, Lasso, ElasticNet
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
@@ -28,8 +28,15 @@ lasso_reg.fit(X_poly, y)
 print(lasso_reg.intercept_, lasso_reg.coef_)
 y_predict_lasso = lasso_reg.predict(X_new_poly)
 
+print('Solution with elastic net regularization mix of rifge and lasso')
+en_reg = ElasticNet(alpha=0.1, l1_ratio=0.5) #l1_ratio = 0 - ridge, 1 - lasso
+en_reg.fit(X_poly, y)
+print(en_reg.intercept_, en_reg.coef_)
+y_predict_en = en_reg.predict(X_new_poly)
+
 
 plt.plot(X_new, y_predict_ridge,
          X_new, y_predict_sgd,
-         X_new, y_predict_lasso)
+         X_new, y_predict_lasso,
+         X_new, y_predict_en)
 plot_dataset(X, y)
