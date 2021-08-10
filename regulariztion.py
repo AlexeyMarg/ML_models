44451ce92aa2dcdf1aed3f6067919827ed799d01
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-10 09:14:25 +0300

    ridge regularization

---
+++ regulariztion.py
@@ -0,0 +1,28 @@
+from dataset_generation import generate_parabola_dataset, plot_dataset
+from sklearn.linear_model import Ridge, SGDRegressor
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+
+X, y = generate_parabola_dataset(100)
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+
+print('Solution with ridge regularization (add norm of parameters squares sum to MSE, solve normal equation)')
+ridge_reg = Ridge(alpha=1, solver='cholesky')
+ridge_reg.fit(X_poly, y)
+print(ridge_reg.intercept_, ridge_reg.coef_)
+X_new = np.linspace(-3, 3, 100).reshape(-1, 1)
+X_new_poly = poly_features.fit_transform(X_new)
+y_predict_ridge = ridge_reg.predict(X_new_poly)
+
+print('Use of SGDRegressor')
+sgd_reg = SGDRegressor(penalty='l2')
+sgd_reg.fit(X_poly, y)
+print(sgd_reg.intercept_, sgd_reg.coef_)
+y_predict_sgd = sgd_reg.predict(X_new_poly)
+
+
+plt.plot(X_new, y_predict_ridge,
+         X_new, y_predict_sgd)
+plot_dataset(X, y)
