c7ee4163d82b46785b6a9f1c2f5032510dcd474d
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-17 15:26:13 +0300

    tensor flow v1 hello world

---
+++ tensor_flow/hello_world.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+x = tf.compat.v1.Variable(3, name='x')
+y = tf.compat.v1.Variable(4, name='x')
+f = x * x * y + y + 2
+
+with tf.compat.v1.Session() as sess:
+    x.initializer.run()
+    y.initializer.run()
+    result = sess.run(f)
+    print(result)
