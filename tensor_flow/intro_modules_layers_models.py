add9a50fe0ece1ca1c859b22dd455ebe2bbb5ded
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-03 12:59:48 +0300

    Update intro_modules_layers_models.py

--- tensor_flow/intro_modules_layers_models.py
+++ tensor_flow/intro_modules_layers_models.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 from datetime import datetime
 
+'''
+Most models are made of layers. Layers are functions with a known mathematical structure that can be reused and have trainable variables. 
+In TensorFlow, most high-level implementations of layers and models, such as Keras or Sonnet, are built on the same foundational class: tf.Module.
+Here's an example of a very simple tf.Module that operates on a scalar tensor:
+'''
+
 class SimpleModule(tf.Module):
     def __init__(self, name=None):
         super().__init__(name=name)
