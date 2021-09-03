f9166b0acebb85d271daf2119a063e13613631f3
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-03 11:02:55 +0300

    Controlling what the tape watches

--- tensor_flow/auto_diff_gradients.py
+++ tensor_flow/auto_diff_gradients.py
@@ -65,3 +65,60 @@ grad = tape.gradient(loss, layer.trainable_variables)
 for var, g in zip(layer.trainable_variables, grad):
     print(f'{var.name}, shape: {g.shape}')
 
+
+'''
+Controlling what the tape watches
+The default behavior is to record all operations after accessing a trainable tf.Variable. The reasons for this are:
+
+The tape needs to know which operations to record in the forward pass to calculate the gradients in the backwards pass.
+The tape holds references to intermediate outputs, so you don't want to record unnecessary operations.
+The most common use case involves calculating the gradient of a loss with respect to all a model's trainable variables.
+For example, the following fails to calculate a gradient because the tf.Tensor is not "watched" by default, and the tf.Variable is not trainable:
+'''
+# A trainable variable
+x0 = tf.Variable(3.0, name='x0')
+# Not trainable
+x1 = tf.Variable(3.0, name='x1', trainable=False)
+# Not a Variable: A variable + tensor returns a tensor.
+x2 = tf.Variable(2.0, name='x2') + 1.0
+# Not a variable
+x3 = tf.constant(3.0, name='x3')
+
+with tf.GradientTape() as tape:
+    y = (x0 ** 2) + (x1 ** 2) + (x2 ** 2)
+
+grad = tape.gradient(y, [x0, x1, x2, x3])
+for g in grad:
+    print(g)
+
+for g in [var.name for var in tape.watched_variables()]:
+    print(g)
+
+# To record gradients with respect to a tf.Tensor, you need to call GradientTape.watch(x):
+
+x = tf.constant(3.0, name='x')
+with tf.GradientTape() as tape:
+    tape.watch(x)
+    y = x ** 2
+print(tape.gradient(y, x))
+
+# Conversely, to disable the default behavior of watching all tf.Variables, set watch_accessed_variables=False
+# when creating the gradient tape. This calculation uses two variables, but only connects the gradient for one of the variables:
+
+
+x0 = tf.Variable(0.0)
+x1 = tf.Variable(10.0)
+
+with tf.GradientTape(watch_accessed_variables=False) as tape:
+    tape.watch(x1)
+    y0 = tf.math.sin(x0)
+    y1 = tf.nn.softplus(x1)
+    y = y0 + y1
+    ys = tf.reduce_sum(y)
+
+# Since GradientTape.watch was not called on x0, no gradient is computed with respect to it:
+# dys/dx1 = exp(x1) / (1 + exp(x1)) = sigmoid(x1)
+grad = tape.gradient(ys, {'x0': x0, 'x1': x1})
+
+print('dy/dx0:', grad['x0'])
+print('dy/dx1:', grad['x1'].numpy())
