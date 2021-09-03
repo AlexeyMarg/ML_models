05a9546aa724af050028014e31bddc28fbb8dc0d
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-03 13:42:44 +0300

    Example of a two-layer linear layer model made out of modules.

--- tensor_flow/intro_modules_layers_models.py
+++ tensor_flow/intro_modules_layers_models.py
@@ -21,3 +21,34 @@ print(simple_module(tf.constant(5.)))
 print("trainable variables:", simple_module.trainable_variables)
 # Every variable
 print("all variables:", simple_module.variables)
+
+'''
+Example of a two-layer linear layer model made out of modules.
+'''
+#First a dense (linear) layer:
+class Dense(tf.Module):
+    def __init__(self, in_features, out_features, name=None):
+        super().__init__(name=name)
+        self.w = tf.Variable( tf.random.normal([in_features, out_features]), name="w")
+        self.b = tf.Variable( tf.zeros([out_features]), name="b")
+    def __call__(self, x):
+        y = tf.matmul(x, self.w) + self.b
+        return tf.nn.relu(y)
+
+class SequentialModule(tf.Module):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+
+        self.dense_1 = Dense(in_features=3, out_features=3)
+        self.dense_2 = Dense(in_features=3, out_features=2)
+
+    def __call__(self, x):
+        x = self.dense_1(x)
+        return self.dense_2(x)
+
+
+my_model = SequentialModule(name="my_module")
+print('Model results: ', my_model(tf.constant([ [2., 2., 2.] ])))
+print('Submodules: ', my_model.submodules)
+for var in my_model.variables:
+    print(var)
