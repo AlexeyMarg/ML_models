62dd087b414bebb492f4806d42c67f78bc235624
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-06 10:50:51 +0300

    keras FlexibleDense

--- tensor_flow/intro_modules_layers_models.py
+++ tensor_flow/intro_modules_layers_models.py
@@ -131,3 +131,33 @@ class MyDense(tf.keras.layers.Layer):
 simple_layer = MyDense(name='simple', in_features=3, out_features=2)
 print('Use of keras layer')
 print(simple_layer([[2.0, 2.0, 2.0]]))
+
+'''
+The build step
+As noted, it's convenient in many cases to wait to create variables until you are sure of the input shape.
+Keras layers come with an extra lifecycle step that allows you more flexibility in how you define your layers. This is defined in the build function.
+build is called exactly once, and it is called with the shape of the input. It's usually used to create variables (weights).
+You can rewrite MyDense layer above to be flexible to the size of its inputs:
+'''
+
+class FlexibleDense(tf.keras.layers.Layer):
+    def __init__(self, out_features, **kwargs):
+        super().__init__(**kwargs)
+        self.out_features = out_features
+
+    # Create the state of the layer (weights)
+    def build(self, input_shape):
+        self.w = tf.Variable(tf.random.normal( [input_shape[-1], self.out_features] ), name='w')
+        self.b = tf.Variable(tf.zeros([self.out_features]), name='b')
+
+    def call(self, inputs):
+        y = tf.matmul(inputs, self.w) + self.b
+        return y
+
+# Create the instance of the layer
+flexible_dense = FlexibleDense(out_features=3)
+print('flexible_dense variables before build: ', flexible_dense.variables)
+
+# Call it, with predictably random results
+print("Model results:", flexible_dense(tf.constant([[2.0, 2.0, 2.0], [3.0, 3.0, 3.0]])))
+#Since build is only called once, inputs will be rejected if the input shape is not compatible with the layer's variables
