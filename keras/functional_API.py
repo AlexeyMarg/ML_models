fd3602b238fd079be4665d3c679185635ce91e4a
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-14 13:28:22 +0300

    Serialization of custom layer

--- keras/functional_API.py
+++ keras/functional_API.py
@@ -201,3 +201,34 @@ inputs = keras.Input((4,))
 outputs = CustomDense(10)(inputs)
 
 model = keras.Model(inputs, outputs)
+
+#For serialization support in your custom layer, define a get_config method that returns the constructor arguments of the layer instance:
+class CustomDense(layers.Layer):
+    def __init__(self, units=32):
+        super(CustomDense, self).__init__()
+        self.units = units
+
+    def build(self, input_shape):
+        self.w = self.add_weight(
+            shape=(input_shape[-1], self.units),
+            initializer="random_normal",
+            trainable=True,
+        )
+        self.b = self.add_weight(
+            shape=(self.units,), initializer="random_normal", trainable=True
+        )
+
+    def call(self, inputs):
+        return tf.matmul(inputs, self.w) + self.b
+
+    def get_config(self):
+        return {"units": self.units}
+
+
+inputs = keras.Input((4,))
+outputs = CustomDense(10)(inputs)
+
+model = keras.Model(inputs, outputs)
+config = model.get_config()
+
+new_model = keras.Model.from_config(config, custom_objects={"CustomDense": CustomDense})
