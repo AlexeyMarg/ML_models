bedbf24a79c52dd6c83d1d74bf0b1a265112e1fe
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-14 15:24:51 +0300

    custom metrics

--- keras/train_eval.py
+++ keras/train_eval.py
@@ -135,3 +135,45 @@ model.compile(optimizer=keras.optimizers.Adam(), loss=CustomMSE())
 
 y_train_one_hot = tf.one_hot(y_train, depth=10)
 model.fit(x_train, y_train_one_hot, batch_size=64, epochs=1)
+
+'''
+Custom metrics
+If you need a metric that isn't part of the API, you can easily create custom metrics by subclassing the tf.keras.metrics.Metric class. You will need to implement 4 methods:
+
+__init__(self), in which you will create state variables for your metric.
+update_state(self, y_true, y_pred, sample_weight=None), which uses the targets y_true and the model predictions y_pred to update the state variables.
+result(self), which uses the state variables to compute the final results.
+reset_states(self), which reinitializes the state of the metric.
+State update and results computation are kept separate (in update_state() and result(), respectively) because in some cases, the results computation might be very expensive and would only be done periodically.
+
+Here's a simple example showing how to implement a CategoricalTruePositives metric that counts how many samples were correctly classified as belonging to a given class:
+'''
+class CategoricalTruePositives(keras.metrics.Metric):
+    def __init__(self, name="categorical_true_positives", **kwargs):
+        super(CategoricalTruePositives, self).__init__(name=name, **kwargs)
+        self.true_positives = self.add_weight(name="ctp", initializer="zeros")
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.reshape(tf.argmax(y_pred, axis=1), shape=(-1, 1))
+        values = tf.cast(y_true, "int32") == tf.cast(y_pred, "int32")
+        values = tf.cast(values, "float32")
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, "float32")
+            values = tf.multiply(values, sample_weight)
+        self.true_positives.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.true_positives
+
+    def reset_states(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.true_positives.assign(0.0)
+
+
+model = get_uncompiled_model()
+model.compile(
+    optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[CategoricalTruePositives()],
+)
+model.fit(x_train, y_train, batch_size=64, epochs=3)
