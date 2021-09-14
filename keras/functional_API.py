1ffa86e7e0816ea243be357ffc5db8716674c168
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-14 12:58:31 +0300

    Models stack

--- keras/functional_API.py
+++ keras/functional_API.py
@@ -38,3 +38,33 @@ model.save('my_model')
 del model
 model = keras.models.load_model('my_model')
 print(model)
+
+'''
+Use the same graph of layers to define multiple models
+In the functional API, models are created by specifying their inputs and outputs in a graph of layers. 
+That means that a single graph of layers can be used to generate multiple models.
+
+In the example below, you use the same stack of layers to instantiate two models: an encoder model that turns image 
+inputs into 16-dimensional vectors, and an end-to-end autoencoder model for training.
+'''
+
+encoder_input = keras.Input(shape=(28, 28, 1), name="img")
+x = layers.Conv2D(16, 3, activation="relu")(encoder_input)
+x = layers.Conv2D(32, 3, activation="relu")(x)
+x = layers.MaxPooling2D(3)(x)
+x = layers.Conv2D(32, 3, activation="relu")(x)
+x = layers.Conv2D(16, 3, activation="relu")(x)
+encoder_output = layers.GlobalMaxPooling2D()(x)
+
+encoder = keras.Model(encoder_input, encoder_output, name="encoder")
+encoder.summary()
+
+x = layers.Reshape((4, 4, 1))(encoder_output)
+x = layers.Conv2DTranspose(16, 3, activation="relu")(x)
+x = layers.Conv2DTranspose(32, 3, activation="relu")(x)
+x = layers.UpSampling2D(3)(x)
+x = layers.Conv2DTranspose(16, 3, activation="relu")(x)
+decoder_output = layers.Conv2DTranspose(1, 3, activation="relu")(x)
+
+autoencoder = keras.Model(encoder_input, decoder_output, name="autoencoder")
+autoencoder.summary()
