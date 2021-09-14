6656fdc447c7f7f6568f6120f480c8154d7f7575
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-09-14 13:16:04 +0300

    Complex topology model

--- keras/functional_API.py
+++ keras/functional_API.py
@@ -15,7 +15,7 @@ outputs = layers.Dense(10)(x)
 model = keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
 model.summary()
 
-#keras.utils.plot_model(model, 'mnist_model.png')
+keras.utils.plot_model(model, 'mnist_model.png')
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 x_train = x_train.reshape(60000, 784).astype('float32') / 255
@@ -68,3 +68,95 @@ decoder_output = layers.Conv2DTranspose(1, 3, activation="relu")(x)
 
 autoencoder = keras.Model(encoder_input, decoder_output, name="autoencoder")
 autoencoder.summary()
+
+'''
+Manipulate complex graph topologies
+Models with multiple inputs and outputs
+The functional API makes it easy to manipulate multiple inputs and outputs. This cannot be handled with the Sequential API.
+
+For example, if you're building a system for ranking customer issue tickets by priority and routing them to the correct department, then the model will have three inputs:
+
+the title of the ticket (text input),
+the text body of the ticket (text input), and
+any tags added by the user (categorical input)
+This model will have two outputs:
+
+the priority score between 0 and 1 (scalar sigmoid output), and
+the department that should handle the ticket (softmax output over the set of departments).
+You can build this model in a few lines with the functional API:
+'''
+
+num_tags = 12  # Number of unique issue tags
+num_words = 10000  # Size of vocabulary obtained when preprocessing text data
+num_departments = 4  # Number of departments for predictions
+
+title_input = keras.Input(
+    shape=(None,), name="title"
+)  # Variable-length sequence of ints
+body_input = keras.Input(shape=(None,), name="body")  # Variable-length sequence of ints
+tags_input = keras.Input(
+    shape=(num_tags,), name="tags"
+)  # Binary vectors of size `num_tags`
+
+# Embed each word in the title into a 64-dimensional vector
+title_features = layers.Embedding(num_words, 64)(title_input)
+# Embed each word in the text into a 64-dimensional vector
+body_features = layers.Embedding(num_words, 64)(body_input)
+
+# Reduce sequence of embedded words in the title into a single 128-dimensional vector
+title_features = layers.LSTM(128)(title_features)
+# Reduce sequence of embedded words in the body into a single 32-dimensional vector
+body_features = layers.LSTM(32)(body_features)
+
+# Merge all available features into a single large vector via concatenation
+x = layers.concatenate([title_features, body_features, tags_input])
+
+# Stick a logistic regression for priority prediction on top of the features
+priority_pred = layers.Dense(1, name="priority")(x)
+# Stick a department classifier on top of the features
+department_pred = layers.Dense(num_departments, name="department")(x)
+
+# Instantiate an end-to-end model predicting both priority and department
+model = keras.Model(
+    inputs=[title_input, body_input, tags_input],
+    outputs=[priority_pred, department_pred],
+)
+
+keras.utils.plot_model(model, "multi_input_and_output_model.png", show_shapes=True)
+
+# When compiling this model, you can assign different losses to each output. You can even assign different weights to each loss -- to modulate their contribution to the total training loss.
+model.compile(
+    optimizer=keras.optimizers.RMSprop(1e-3),
+    loss=[
+        keras.losses.BinaryCrossentropy(from_logits=True),
+        keras.losses.CategoricalCrossentropy(from_logits=True),
+    ],
+    loss_weights=[1.0, 0.2],
+)
+
+#Since the output layers have different names, you could also specify the losses and loss weights with the corresponding layer names:
+model.compile(
+    optimizer=keras.optimizers.RMSprop(1e-3),
+    loss={
+        "priority": keras.losses.BinaryCrossentropy(from_logits=True),
+        "department": keras.losses.CategoricalCrossentropy(from_logits=True),
+    },
+    loss_weights={"priority": 1.0, "department": 0.2},
+)
+
+#Train the model by passing lists of NumPy arrays of inputs and targets:
+# Dummy input data
+title_data = np.random.randint(num_words, size=(1280, 10))
+body_data = np.random.randint(num_words, size=(1280, 100))
+tags_data = np.random.randint(2, size=(1280, num_tags)).astype("float32")
+
+# Dummy target data
+priority_targets = np.random.random(size=(1280, 1))
+dept_targets = np.random.randint(2, size=(1280, num_departments))
+
+model.fit(
+    {"title": title_data, "body": body_data, "tags": tags_data},
+    {"priority": priority_targets, "department": dept_targets},
+    epochs=2,
+    batch_size=32,
+)
