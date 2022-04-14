28409da3903a79ce0f14fe66433b7743e2cc51e0
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2022-04-14 13:48:09 +0300

    fix import linear_regression

--- tensor_flow/7_tf_linear_regression.py
+++ tensor_flow/7_tf_linear_regression.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from sklearn.datasets import fetch_california_housing
 import numpy as np
 
@@ -12,7 +13,7 @@ X = tf.constant(housing_biased, dtype=tf.float32, name='X')
 y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name='y')
 
 XT = tf.transpose(X)
-theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+theta = tf.matmul(tf.matmul(tf.linalg.inv(tf.matmul(XT, X)), XT), y)
 
 print(theta)
 
