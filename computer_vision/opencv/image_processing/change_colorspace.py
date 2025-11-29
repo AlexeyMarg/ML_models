6b7e527f5c7715d023b0b75fd82fc56638320669
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2025-11-29 21:01:16 +0300

    opencv_basic_templates

---
+++ computer_vision/opencv/image_processing/change_colorspace.py
@@ -0,0 +1,33 @@
+'''
+Tracking of objecct with specified color
+'''
+
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([35, 50, 50])  
+    upper_green = np.array([85, 255, 255])
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('result', res)
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+    
+cv2.destroyAllWindows()
+    
+    
+
+
